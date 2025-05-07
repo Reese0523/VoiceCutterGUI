69fa9dbb68bd2445225e55f4813d3c69dddc85a9
Reese0523 <103919536+Reese0523@users.noreply.github.com>
2025-05-07 21:24:05 +0800

    Update build_nuitka.bat

--- build_nuitka.bat
+++ build_nuitka.bat
@@ -6,7 +6,7 @@ echo Installing/upgrading pip and dependencies...
 %PY% -m pip install nuitka moviepy whisper tqdm numpy imageio_ffmpeg
 
 echo Packaging with Nuitka (one-dir)...
-%PY% -m nuitka --standalone --onefile-off --windows-console-mode=attach --enable-plugin=tk-inter --assume-yes-for-downloads --include-package=moviepy --include-package=whisper --include-package=imageio_ffmpeg voice_cutter_gui.py
+%PY% -m nuitka --standalone --windows-console-mode=attach --enable-plugin=tk-inter --assume-yes-for-downloads --include-package=moviepy --include-package=whisper --include-package=imageio_ffmpeg voice_cutter_gui.py
 
 if exist "%cd%\dist\voice_cutter_gui.dist\voice_cutter_gui.exe" (
     echo Build succeeded: dist\voice_cutter_gui.dist\voice_cutter_gui.exe
