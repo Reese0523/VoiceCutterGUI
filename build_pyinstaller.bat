9bce8fe42e873edfcd934575227e5b81c398db83
Reese0523 <103919536+Reese0523@users.noreply.github.com>
2025-05-08 02:51:31 +0800

    Update build_pyinstaller.bat

--- build_pyinstaller.bat
+++ build_pyinstaller.bat
@@ -3,10 +3,9 @@ set PY=python
 %PY% -m pip install --upgrade pip
 %PY% -m pip install pyinstaller moviepy whisper tqdm numpy imageio_ffmpeg
 
-pyinstaller --clean --noupx --onedir --console voice_cutter_gui.spec
+pyinstaller --clean --noupx --onedir --console voice_cutter_gui.spec > build.log 2>&1
 if exist dist\voice_cutter_gui.dist\voice_cutter_gui.exe (
     echo Build OK
 ) else (
-    echo Build Failed, see log
+    echo Build Failed, see build.log
 )
-pause
